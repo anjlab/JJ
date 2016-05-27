a80856b36c08dd71faaba85b277e7f7176c4779e
Yury Korolev <yury.korolev@gmail.com>
2016-05-27 12:07:59 +0300

    Add Carthage support

---
+++ Example/JJ/JJ.h
@@ -0,0 +1,19 @@
+//
+//  JJ.h
+//  JJ
+//
+//  Created by Yury Korolev on 5/27/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJ.
+FOUNDATION_EXPORT double JJVersionNumber;
+
+//! Project version string for JJ.
+FOUNDATION_EXPORT const unsigned char JJVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJ/PublicHeader.h>
+
+
