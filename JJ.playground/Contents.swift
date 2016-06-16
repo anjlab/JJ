1b3651073cd5cb013a79ff17a1110e8d8712c404
Roman <tutubalinry@gmail.com>
2016-06-16 17:33:59 +0300

    JJ playground

---
+++ JJ.playground/Contents.swift
@@ -0,0 +1,150 @@
+import Foundation
+/*:
+ # JJ
+ 
+ Super simple json parser for Swift
+ 
+ ### Requirements
+ 
+ Do depencies. You can copy JJ.swift into your project if you want.
+ 
+ ### Installation
+ 
+ JJ is available through [CocoaPods](http://cocoapods.org). To install
+ it, simply add the following line to your Podfile:
+ 
+ ```ruby
+ pod "JJ"
+ ```
+ 
+ ### Example
+ */
+struct Branch {
+    let branch: String
+}
+
+struct Repository {
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String?
+    let sometimesMissingKey: String?
+    
+    let owner: Data
+    let defaultBranch: Branch
+    
+    init(anyObject: AnyObject?) throws {
+        let obj = try jj(anyObject).obj()
+        self.name = obj["name"].toString()
+        self.description = obj["description"].toString()
+        self.stargazersCount = obj["stargazersCount"].toInt()
+        self.language = obj["language"].asString
+        self.sometimesMissingKey = obj["sometimesMissingKey"].asString
+        
+        let user = try obj["owner"].obj()
+        let name = try user.at("name").string()
+        let headquarters = try user.at("headquarters").string()
+        
+        let data = NSMutableData()
+        let coder = NSKeyedArchiver(forWritingWith: data)
+        let enc = jj(encoder: coder)
+        enc.put(name, at: "name")
+        enc.put(headquarters, at: "headquarters")
+        coder.finishEncoding()
+        
+        self.owner = data as Data
+        self.defaultBranch = Branch(branch: obj["branch"].toString())
+    }
+    
+    func printFullName() {
+        let decoder = NSKeyedUnarchiver(forReadingWith: self.owner)
+        let dec = jj(decoder: decoder)
+        let ownerName = dec["name"].asString
+        print("\(ownerName)/\(name)")
+    }
+}
+
+
+let json = [
+    "name" : "JJ",
+    "description" : "Super simple json parser for Swift",
+    "stargazersCount" : 999999,
+    "language" : "RU",
+    "sometimesMissingKey" : NSNull(),
+    "owner" : [
+        "name" : "Yury",
+        "headquarters" : "AnjLab"
+    ],
+    "branch" : "master"
+]
+
+do {
+    let repository = try Repository(anyObject: json)
+    repository.printFullName()
+} catch {
+    debugPrint(error)
+}
+/*:
+ 
+ ### Features
+ - Informative errors
+ - Decoding depends on inferred type
+ - Leverages Swift 2's error handling
+ - Support classes conforming ```NSCoding```
+ 
+ ### Parsing Types
+ - `Bool`
+ - `Int` & `UInt`
+ - `Float`
+ - `Double`
+ - `NSNumber`
+ - `String`
+ - `NSDate`
+ - `NSURL`
+ - `NSTimeZone`
+ - [`AnyObject`]
+ - [`String` : `AnyObject`]
+ 
+ ### Errors
+ `JJError` conforming `ErrorType` and there are currently two error-structs conforming to it
+ - `WrongType` throws when it is impossible to convert the element
+ - `NotFound` throws if the element is missing
+ */
+let j = ["element"]
+
+do {
+    let _ = try jj(j).obj()
+} catch {
+    print(error)
+}
+/*:
+ ### Handling Errors
+ Expressions like `.<Type>()` will throw directly, and catch-statements can be used to create the most complex error handling behaviours. This also means that `try?` can be used to return nil if anything goes wrong instead of throwing.
+ 
+ For required values is most useful methods `.to<Type>(defaultValue)`. If the value is missing or does not match its type, will be used the default value.
+ 
+ For optional values there's methods `.as<Type>`.
+ 
+ | Method | Examples | Null Behaviour | Missing Key Behaviour | Type Mismatch Behaviour |
+ | --- | :---: | :---: | :---: | :---: |
+ | `.<Type>()` | `.int()` | `throws` | `throws` | `throws` |
+ | `.to<Type>(defaultValue)` | `.toString()` or `.toString("Default")` | `defaultValue` | `defaultValue` | `defaultValue` |
+ | `.as<Type>` | `.asObj` | `nil` | `nil` | `nil` |
+ | `.decode()` | `.decode() as NSNumber` | `throws` | `throws` | `throws` |
+ | `.decodeAs()` | `.decodeAs()` | `nil` | `nil` | `nil` |
+ 
+ 
+ ### Requirements
+ - iOS 8.0+ / Mac OS X 10.10+ / tvOS 9.0+ / watchOS 2.0+
+ - Xcode 7.3
+ - Swift 2.2
+ 
+ ### Author
+ 
+ Yury Korolev, yury.korolev@gmail.com
+ 
+ ### License
+ 
+ JJ is available under the MIT license. See the LICENSE file for more info.
+ */
+
---
+++ JJ.playground/Sources/JJ.swift
@@ -0,0 +1,542 @@
+//
+//  JJ.swift
+//  Pods
+//
+//  Created by Yury Korolev on 5/27/16.
+//
+//
+
+import Foundation
+
+private let _rfc3339DateFormatter: DateFormatter = _buildRfc3339DateFormatter()
+
+private func _buildRfc3339DateFormatter() -> DateFormatter {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(localeIdentifier: "en_US")
+    formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"
+    formatter.timeZone = TimeZone(forSecondsFromGMT: 0)
+    return formatter
+}
+
+public extension String {
+    func asRFC3339Date() -> Date? {
+        return _rfc3339DateFormatter.date(from: self)
+    }
+}
+
+public extension Date {
+    func toRFC3339String() -> String {
+        return _rfc3339DateFormatter.string(from: self)
+    }
+}
+
+public enum JJError: ErrorProtocol, CustomStringConvertible {
+    case wrongType(v: AnyObject?, path: String, toType: String)
+    case notFound(path: String)
+
+    public var description: String {
+        switch self {
+        case let .wrongType(v: v, path: path, toType: type):
+            return "JJError.WrongType: Can't convert \(v) at path: '\(path)' to type '\(type)'"
+        case let .notFound(path: path):
+            return "JJError.NotFound: No object at path: '\(path)'"
+        }
+    }
+}
+
+public func jj(_ v: AnyObject?) -> JJVal { return JJVal(v) }
+
+public func jj(decoder: NSCoder) -> JJDec { return JJDec(decoder) }
+
+public func jj(encoder: NSCoder) -> JJEnc { return JJEnc(encoder) }
+
+public struct JJArr: CustomDebugStringConvertible {
+    private let _path: String
+    private let _v: [AnyObject]
+
+    public init(_ v: [AnyObject], path: String) {
+        _v = v
+        _path = path
+    }
+
+    public subscript (index: Int) -> JJVal {
+        return at(index)
+    }
+
+    public func at(_ index: Int) -> JJVal {
+        let newPath = _path + "[\(index)]"
+
+        if index >= 0 && index < _v.count {
+            return JJVal(_v[index], path: newPath)
+        }
+        return JJVal(nil, path: newPath)
+    }
+
+    // MARK: extension point
+    public var raw: [AnyObject] { return _v }
+    public var path: String { return _path }
+
+    // MARK: Shortcusts
+
+    public var exists: Bool { return true }
+    public var count:Int { return _v.count }
+
+    public func prettyPrint(space: String = "", spacer: String = "  ") -> String {
+        if _v.count == 0 {
+            return "[]"
+        }
+        var str = "[\n"
+        let nextSpace = space + spacer
+        for v in _v {
+            str += "\(nextSpace)" + jj(v).prettyPrint(space: nextSpace, spacer: spacer) + ",\n"
+        }
+        str.remove(at: str.index(str.endIndex, offsetBy: -2))
+        
+        return str + "\(space)]"
+    }
+
+    public var debugDescription: String { return prettyPrint() }
+}
+
+public struct JJMaybeArr {
+    private let _path: String
+    private let _v: JJArr?
+
+    public init(_ v: JJArr?, path: String) {
+        _v = v
+        _path = path
+    }
+
+    public func at(_ index: Int) -> JJVal {
+        return _v?.at(index) ?? JJVal(nil, path: _path + "<nil>[\(index)]")
+    }
+
+    public subscript (index: Int) -> JJVal { return at(index) }
+
+    public var exists: Bool { return _v != nil }
+
+    // MARK: extension point
+    public var raw: [AnyObject]? { return _v?.raw }
+    public var path: String { return _path }
+
+}
+
+public struct JJObj: CustomDebugStringConvertible {
+    private let _path: String
+    private let _v: [String: AnyObject]
+
+    public init(_ v: [String: AnyObject], path: String) {
+        _v = v
+        _path = path
+    }
+
+    public func at(_ key: String) -> JJVal {
+        let newPath = _path + ".\(key)"
+        #if DEBUG
+            if let msg = _v["$\(key)__depricated"] {
+                debugPrint("WARNING:!!!!. Using depricated field \(newPath): \(msg)")
+            }
+        #endif
+        return JJVal(_v[key], path: newPath)
+    }
+
+    public subscript (key: String) -> JJVal { return at(key) }
+
+    // MARK: extension point
+    public var raw: [String: AnyObject] { return _v }
+    public var path: String { return _path }
+
+    // Shortcusts
+
+    public var exists: Bool { return true }
+    public var count:Int { return _v.count }
+
+    public func prettyPrint(space: String = "", spacer: String = "  ") -> String {
+        if _v.count == 0 {
+            return "{}"
+        }
+        var str = "{\n"
+        for (k, v) in _v {
+            let nextSpace = space + spacer
+            str += "\(nextSpace)\"\(k)\": \(jj(v).prettyPrint(space: nextSpace, spacer: spacer)),\n"
+        }
+        str.remove(at: str.index(str.endIndex, offsetBy: -2))
+        return str + "\(space)}"
+    }
+
+    public var debugDescription: String { return prettyPrint() }
+}
+
+public struct JJMaybeObj {
+    private let _path: String
+    private let _v: JJObj?
+
+    public init(_ v: JJObj?, path: String) {
+        _v = v
+        _path = path
+    }
+
+    public func at(_ key: String) -> JJVal {
+        return _v?.at(key) ?? JJVal(nil, path: _path + "<nil>.\(key)")
+    }
+
+    public subscript (key: String) -> JJVal { return at(key) }
+
+
+    // MARK: extension point
+    public var raw: [String: AnyObject]? { return _v?.raw }
+    public var path: String { return _path }
+
+    // MARK: shortcusts
+
+    public var exists: Bool { return _v != nil }
+}
+
+public struct JJVal: CustomDebugStringConvertible {
+    private let _path: String
+    private let _v: AnyObject?
+
+    public init(_ v: AnyObject?, path: String = "<root>") {
+        _v = v
+        _path = path
+    }
+
+    // MARK: Bool
+
+    public var asBool: Bool? { return _v as? Bool }
+
+    public func toBool(_ defaultValue:  @autoclosure() -> Bool = false) -> Bool {
+        return asBool ?? defaultValue()
+    }
+
+
+    public func bool() throws -> Bool {
+        if let x = _v as? Bool {
+            return x
+        }
+        throw JJError.wrongType(v: _v, path: _path, toType: "Bool")
+    }
+
+    // MARK: Int
+
+    public var asInt: Int? { return _v as? Int }
+
+    public func toInt(_ defaultValue:  @autoclosure() -> Int = 0) -> Int {
+        return asInt ?? defaultValue()
+    }
+
+    public func int() throws -> Int {
+        if let x = asInt {
+            return x
+        }
+        throw JJError.wrongType(v: _v, path: _path, toType: "Int")
+    }
+
+    // MARK: UInt
+
+    public var asUInt: UInt? { return _v as? UInt }
+
+    public func toUInt(_ defaultValue:  @autoclosure() -> UInt = 0) -> UInt {
+        return asUInt ?? defaultValue()
+    }
+
+    public func uInt() throws -> UInt {
+        if let x = asUInt {
+            return x
+        }
+        throw JJError.wrongType(v: _v, path: _path, toType: "UInt")
+    }
+
+    // MARK: Number
+
+    public var asNumber: NSNumber? { return _v as? NSNumber }
+
+    public func toNumber(_ defaultValue:  @autoclosure() -> NSNumber = 0) -> NSNumber {
+        return asNumber ?? defaultValue()
+    }
+
+    public func number() throws -> NSNumber {
+        if let x = asNumber {
+            return x
+        }
+        throw JJError.wrongType(v: _v, path: _path, toType: "Number")
+    }
+
+    // MARK: Float
+
+    public var asFloat: Float? { return _v as? Float }
+
+    public func toFloat(_ defaultValue:  @autoclosure() -> Float = 0) -> Float {
+        return asFloat ?? defaultValue()
+    }
+
+    public func float() throws -> Float {
+        if let x = asFloat {
+            return x
+        }
+        throw JJError.wrongType(v: _v, path: _path,toType: "Float")
+    }
+
+    // MARK: Double
+
+    public var asDouble: Double? { return _v as? Double }
+
+    public func toDouble(_ defaultValue:  @autoclosure() -> Double = 0) -> Double {
+        return asDouble ?? defaultValue()
+    }
+
+    public func double() throws -> Double {
+        if let x = asDouble {
+            return x
+        }
+        throw JJError.wrongType(v: _v, path: _path,toType: "Double")
+    }
+
+
+    // MARK: Object
+
+    public var asObj: JJObj? {
+        if let obj = _v as? [String: AnyObject] {
+            return JJObj(obj, path: _path)
+        }
+
+        return nil
+    }
+
+    public func toObj() -> JJMaybeObj {
+        return JJMaybeObj(asObj, path: _path)
+    }
+
+    public func obj() throws -> JJObj {
+        if let obj = _v as? [String: AnyObject] {
+            return JJObj(obj, path: _path)
+        }
+
+        throw JJError.wrongType( v: _v, path: _path, toType: "[String: AnyObject]")
+    }
+
+    // MARK: Array
+
+    public var asArr: JJArr? {
+        if let arr = _v as? [AnyObject] {
+            return JJArr(arr, path: _path)
+        }
+        return nil
+    }
+
+    public func toArr() -> JJMaybeArr {
+        return JJMaybeArr(asArr, path: _path)
+    }
+
+    public func arr() throws -> JJArr {
+        guard let arr = _v as? [AnyObject] else {
+            throw JJError.wrongType( v: _v, path: _path, toType: "[AnyObject]")
+        }
+        return JJArr(arr, path: _path)
+    }
+
+    // MARK: String
+
+    public var asString: String? { return _v as? String }
+
+    public func toString(_ defaultValue:  @autoclosure() -> String = "") -> String {
+        return asString ?? defaultValue()
+    }
+
+    public func string() throws -> String {
+        if let x = asString {
+            return x
+        }
+        throw JJError.wrongType(v: _v, path: _path, toType: "String")
+    }
+
+    // MARK: Date
+
+    public var asDate: Date? { return asString?.asRFC3339Date() }
+
+    public func date() throws -> Date {
+        if let d = asString?.asRFC3339Date() {
+            return d
+        } else {
+            throw JJError.wrongType(v: _v, path: _path, toType: "Date")
+        }
+    }
+
+    // MARK: URL
+
+    public var asURL: URL? {
+        if let s = asString, d = URL(string: s) {
+            return d
+        } else {
+            return nil
+        }
+    }
+
+    public func toURL(_ defaultValue:  @autoclosure() -> URL = URL(string: "")!) -> URL {
+        return asURL ?? defaultValue()
+    }
+
+    public func url() throws -> URL {
+        if let s = asString, d = URL(string: s) {
+            return d
+        } else {
+            throw JJError.wrongType(v: _v, path: _path, toType: "URL")
+        }
+    }
+
+    // MARK: Null
+
+    public var isNull: Bool { return _v === NSNull() }
+
+
+    // MARK: TimeZone
+
+    public var asTimeZone: TimeZone? {
+        if let s = asString, d = TimeZone(name: s) {
+            return d
+        } else {
+            return nil
+        }
+    }
+
+    // MARK: Navigation as Object
+
+    public func at(_ key: String) -> JJVal {
+        return toObj()[key]
+    }
+
+    public subscript (key: String) -> JJVal {
+        return at(key)
+    }
+
+    // MARK: Navigation as Array
+
+    public func at(_ index: Int) -> JJVal {
+        return toArr()[index]
+    }
+
+    public subscript (index: Int) -> JJVal {
+        return at(index)
+    }
+
+    public var exists: Bool { return _v != nil }
+
+    // MARK: extension point
+
+    public var path: String { return _path }
+    public var raw: AnyObject? { return _v }
+
+    // MARK: pretty print
+
+    public func prettyPrint(space: String = "", spacer: String = "  ") -> String {
+        if let arr = asArr {
+            return arr.prettyPrint(space: space, spacer: spacer)
+        } else if let obj = asObj {
+            return obj.prettyPrint(space: space, spacer: spacer)
+        } else if let s = asString {
+            return "\"\(s)\""
+        } else if isNull {
+            return "null"
+        } else if let v = _v {
+            return v.description
+        } else {
+            return "nil"
+        }
+    }
+
+    // MARK: CustomDebugStringConvertible
+
+    public var debugDescription: String {
+        return prettyPrint()
+    }
+}
+
+public struct JJEnc {
+    private let _enc: NSCoder
+
+    public init(_ enc: NSCoder) {
+        _enc = enc
+    }
+
+    public func put(int v:Int, at: String) {
+        _enc.encode(Int32(v), forKey: at)
+    }
+
+    public func put(bool v:Bool, at: String) {
+        _enc.encode(v, forKey: at)
+    }
+
+    public func put(_ v:AnyObject?, at: String) {
+        _enc.encode(v, forKey: at)
+    }
+}
+
+public struct JJDecVal {
+    private let _key: String
+    private let _dec: NSCoder
+
+    public init(dec: NSCoder, key: String) {
+        _key = key
+        _dec = dec
+    }
+
+    public func string() throws -> String {
+        let v = _dec.decodeObject(forKey: _key)
+        if let x = v as? String {
+            return x
+        }
+        throw JJError.wrongType(v: v, path: _key, toType: "String")
+    }
+
+    public var asString: String? { return _dec.decodeObject(forKey: _key) as? String }
+
+    public func int() throws -> Int { return Int(_dec.decodeInt32(forKey: _key)) }
+
+    public var asInt: Int? { return _dec.decodeObject(forKey: _key) as? Int }
+
+    public var asDate: Date? { return _dec.decodeObject(forKey: _key) as? Date }
+
+    public var asURL: URL? { return _dec.decodeObject(forKey: _key) as? URL }
+
+    public var asTimeZone: TimeZone? { return _dec.decodeObject(forKey: _key) as? TimeZone }
+
+    public func bool() throws -> Bool { return _dec.decodeBool(forKey: _key) }
+
+    public func toBool() -> Bool { return _dec.decodeBool(forKey: _key) }
+
+    public func decodeAs<T: NSCoding>() -> T? { return _dec.decodeObject(forKey: _key) as? T }
+    
+    public func decode<T: NSCoding>() throws -> T {
+        let obj = _dec.decodeObject(forKey: _key)
+        if let v:T = obj as? T {
+            return v
+        }
+       
+        // TODO: find a way to get type
+        throw JJError.wrongType(v: obj, path: _key, toType: "T")
+    }
+
+    public func date() throws -> Date {
+        let v = _dec.decodeObject(forKey: _key)
+        if let x = v as? Date {
+            return x
+        }
+        throw JJError.wrongType(v: v, path: _key, toType: "Date")
+    }
+    
+    // extension point
+    public var decoder: NSCoder { return _dec }
+    public var key: String { return _key }
+}
+
+public struct JJDec {
+    private let _dec: NSCoder
+
+    public init(_ dec: NSCoder) {
+        _dec = dec
+    }
+
+    public subscript (key: String) -> JJDecVal {
+        return JJDecVal(dec: _dec, key: key)
+    }
+}
